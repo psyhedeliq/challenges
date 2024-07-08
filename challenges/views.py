7f94315dcbf1b9dc44c9288489fd91c56b987816
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-08 17:21:02 +0300

    feat: Add URLs for monthly challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def january(request):
+    return HttpResponse("Eat only meet for the entire month")
+
+
+def february(request):
+    return HttpResponse("Work out for 30 minutes each day")
